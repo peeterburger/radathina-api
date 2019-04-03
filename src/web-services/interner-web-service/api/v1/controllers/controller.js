40e0bfc71183a6a707a9c78a127ed31ab32a8266
peeterburger <peter2000.burger2000@gmail.com>
2019-04-03 11:41:55 +0200

    JSON verbessert, Router gefixt

--- src/web-services/interner-web-service/api/v1/controllers/controller.js
+++ src/web-services/interner-web-service/api/v1/controllers/controller.js
@@ -7,5 +7,30 @@ exports.get_attractions_all = function(req, res) {
     for(var entry in database) {
         result.push(entry);
     }
-    res.send();
+    res.json(result);
+};
+
+exports.get_attractions_by_name = function(res, req) {
+    var result = {};
+    for(var entry in database) {
+        if(entry.name === req.params.attr_name){
+            result.push(entry);
+        }
+    }
+    res.json(result);
+};
+
+exports.get_attractions_nearby = function(res, req) {
+    for(var sehenswürdigkeit in database) {
+
+    }
+};
+
+exports.delete_attraction_by_name = function(res, req) {
+    for(var entry in database) {
+        if(entry.name === req.params.attr_name){
+            res.json({ message: 'Task successfully deleted' });
+            break;
+        }
+    }
 };
--- src/web-services/interner-web-service/api/v1/routes/routes.js
+++ src/web-services/interner-web-service/api/v1/routes/routes.js
@@ -1,29 +1,14 @@
 'use strict';
 
-const express = require('express');
-const router = express.Router();
-
-router.use(function timeLog(req, res, next) {
-    console.log('Time: ', Date.now());
-    next();
-});
-
-// define the home page route
-router.get('/', function(req, res) {
-    res.send('Home');
-});
-
 module.exports = function(app) {
     var controller = require('../controllers/controller');
 
     app.route('/attractions')
-        .get(controller.list_all_attractions)
-        .post()
+        .get(controller.get_attractions_all);
 
     app.route('/attractions/:attr_name')
-        .get(controller.list_attractions_by_name)
-        .delete(controller.delete_attraction_by_name);
+        .get(controller.get_attractions_by_name);
 
     app.route('/attractions/:current_position_x/:current_position_y/:radius')
-        .get(controller.list_attractions_nearby)
-}
+        .get(controller.get_attractions_nearby);
+};
