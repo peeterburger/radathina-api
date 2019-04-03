0edad87e4777166614504327ead86737b8d517a5
peeterburger <peter2000.burger2000@gmail.com>
2019-04-03 23:21:02 +0200

    Logger hinzugefügt, Sehenswürdigkeiten x-y zu lon-lat

--- src/web-services/interner-web-service/server.js
+++ src/web-services/interner-web-service/server.js
@@ -2,9 +2,33 @@
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const {transports, createLogger, format} = require('winston');
 
 const app = express();
 const port = process.env.port || 3000;
+
+const logger = createLogger({
+    format: format.combine(
+        format.timestamp(),
+        format.json()
+    ),
+    transports: [
+        new transports.Console()
+    ]
+})
+
+function logRequest(req, res, next) {
+    logger.info(req.url);
+    next();
+}
+
+function logError (err, req, res, next) {
+    logger.error(err);
+    next();
+}
+
+app.use(logRequest);
+app.use(logError);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
