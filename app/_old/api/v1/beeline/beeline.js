20bc6829068c0578089f1bf826b292ef159f671f
peeterburger <peter2000.burger2000@gmail.com>
2019-04-18 17:24:33 +0200

    Alte Dateien entfernt

--- app/_old/api/v1/beeline/beeline.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const router = require('express').Router();
-const controller = require('./_controller/controller')
-
-router.get('/', controller.get_parameter_list);
-router.get('/calculate', controller.calculate_beeline);
-
-module.exports = router;
