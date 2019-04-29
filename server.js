14df1108cda3aa749a572071c1b3651674134c85
peeterburger <peter2000.burger2000@gmail.com>
2019-04-29 11:45:29 +0200

    Default Werte beim Server eingefügt

--- server.js
+++ server.js
@@ -5,28 +5,38 @@ const app = express();
 const mongoose = require('mongoose');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 const server = require('http').Server(app);
 
 const constants = require('./app/config/constants')
 
-const mongodb_host = constants.DEFAULT_MONGODB_HOST;
-const mongodb_port = constants.DEFAULT_MONGODB_PORT;
-const mongodb_database = constants.DEFAULT_MONGODB_DATABSE;
+const mongodb_host = constants.DEFAULT_MONGODB_HOST || 'localhost';
+const mongodb_port = constants.DEFAULT_MONGODB_PORT || 27017;
+const mongodb_database = constants.DEFAULT_MONGODB_DATABSE || 'radathina';
 
-mongoose.connect(`mongodb://${mongodb_host}:${mongodb_port}/${mongodb_database}`,
-	{useNewUrlParser: true});
+mongoose.connect(`mongodb://${mongodb_host}:${mongodb_port}/${mongodb_database}`, {
+	useNewUrlParser: true
+});
 
 app.use(morgan('dev'));
+app.use(morgan('common', {
+	stream: fs.createWriteStream('./logs/access.log', {
+		flags: 'a'
+	})
+}))
+
 app.use(bodyParser.json());
-app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
+app.use(bodyParser.json({
+	type: 'application/vnd.api+json'
+}));
 
 // Die API befindet sich auf [baseUrl]/api/v1
 app.use('/api/v1', require('./app/routes'));
 
-const server_port = process.env.PORT || constants.DEFAULT_SERVER_PORT;
-const server_ip = process.env.IP || constants.DEFAULT_SERVER_IP;
+const server_port = process.env.PORT || constants.DEFAULT_SERVER_PORT || 12345;
+const server_ip = process.env.IP || constants.DEFAULT_SERVER_IP || '0.0.0.0';
 
-server.listen(server_port, server_ip, function() {
+server.listen(server_port, server_ip, function () {
 	const addr = server.address();
 	console.log('Server listening at', addr.address + ':' + addr.port);
 });
